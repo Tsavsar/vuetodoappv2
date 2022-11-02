866d5e19a9101b72921f2b27b69057ebdab98ece
Tsavsar <shatermt@gmail.com>
2022-11-02 13:05:02 +0100

    v2

---
+++ Vuetodolist/src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { reactive, ref, computed } from "vue";
+import Sidebar from "./components/sidebar.vue";
+import categories from "./components/categories.vue";
+import todolist from "./components/todolist.vue";
+import homeVue from "./home.vue";
+import loginVue from "./login.vue";
+
+const routes = {
+  "/": loginVue,
+  "/home": homeVue
+};
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || "/"];
+});
+const nav = ref(false);
+
+function toggle() {
+  nav.value = !nav.value;
+}
+</script>
+
+<template>
+  <component :is="currentView" />
+</template>
+
+<style>
+body {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #f2f2f2;
+  overflow: hidden;
+  padding: 20px;
+  z-index: 0;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  overflow-y: scroll;
+}
+</style>
